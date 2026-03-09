0a7611ba17647451f1fdf772e48cc738e4afc70f
Mateus-Alencar <mateus.alencar3182@outlook.com>
2026-03-08 22:04:54 -0300

    Foi alterado a forma de exibicao do input

--- src/main.py
+++ src/main.py
@@ -1,10 +1,25 @@
 class Grafo:
   def __init__(self, vertices):
     self.vertices = vertices
+
+  def montar_lista_adjacências(self):
+    resultado = []
+    # Separar cada par usando a vírgula.
+    for par in self.vertices.split(","):
+      # par.strip() -> Remove espaços extras no começo e fim da string
+      # .split() -> divide a string em uma lista usando o espaço como separador
+      # map(int) aplica a função int() em cada elemento da lista
+      a, b = map(int, par.strip().split())
+      resultado.append({a: b})
+    print(resultado)
     
 print("--------------------------------------------------------------------")
 print("Implementação de matriz de adjacências para grafos e dígrafos.")
 vertices = input("Informe as arestas do grafo (origem destino,origem destino, ...): ")
+
+montarLista = Grafo(vertices)
+montarLista.montar_lista_adjacências()
+
 print("----------------------------- Execução -----------------------------")
 print("Matriz de Adjacências para Grafo:")
 print("""
